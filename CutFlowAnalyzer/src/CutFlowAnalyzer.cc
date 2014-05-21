73d8bc4f424abed2e31a1bc4c856c0f311688051
pakhotin <pakhotin@gmail.com>
2014-05-20 23:27:33 -0500

    some changes

--- CutFlowAnalyzer/src/CutFlowAnalyzer.cc
+++ CutFlowAnalyzer/src/CutFlowAnalyzer.cc
@@ -81,9 +81,7 @@
 #include "TrackPropagation/SteppingHelixPropagator/interface/SteppingHelixPropagator.h"
 
 #include "MuJetAnalysis/DataFormats/interface/MultiMuon.h"
-//#include "MuJetAnalysis/DataFormats/interface/eig3.h"
-//#include "MuJetAnalysis/DataFormats/src/eig3.cc"
-#include "MuJetAnalysis/CutFlowAnalyzer/interface/ConsistentVertexesCalculator.h"
+#include "MuJetAnalysis/AnalysisTools/interface/ConsistentVertexesCalculator.h"
 
 //******************************************************************************
 //              Auxiliary function: Order objects by pT                         
