f6ce1d340bd2fa804b8c156505c195ae631811cd
Sven Dildick <sven.dildick@cern.ch>
2015-02-02 20:29:13 +0100

    define MuJetProducer in MuJetProducer_cff

--- MuJetProducer/python/MuJetProducer_cff.py
+++ MuJetProducer/python/MuJetProducer_cff.py
@@ -5,5 +5,29 @@ from TrackingTools.TransientTrack.TransientTrackBuilder_cfi import *
 from Configuration.Geometry.GeometryIdeal_cff import *
 from Configuration.StandardSequences.MagneticField_cff import *
 from TrackingTools.GeomPropagators.SmartPropagator_cff import *
-
 from MuJetAnalysis.MuJetProducer.MuJetProducer_cfi import *
+
+TrackerMuJetProducer05 = MuJetProducer.clone(
+    maxMass = cms.double(5.),
+    muons = cms.InputTag("cleanPatTrackerMuonsTriggerMatch"),
+    selectTrackerMuons = cms.bool(True),
+    selectGlobalMuons = cms.bool(False),
+    groupingMode = cms.string("GroupByMassAndVertexProbOrDeltaR"),
+    maxDeltaR = cms.double(0.01),
+    minSegmentMatches = cms.int32(2),
+    minTrackerHits = cms.int32(8),
+    maxTrackerNormChi2 = cms.double(4.0)
+)
+
+PFMuJetProducer05 = MuJetProducer.clone(
+    maxMass = cms.double(5.),
+    muons = cms.InputTag("cleanPatPFMuonsTriggerMatch"),
+    selectTrackerMuons = cms.bool(False),
+    selectGlobalMuons = cms.bool(False),
+    groupingMode = cms.string("GroupByMassAndVertexProbOrDeltaR"),
+    maxDeltaR = cms.double(0.01),
+#    minSegmentMatches = cms.int32(2),
+    minSegmentMatches = cms.int32(-1),
+    minTrackerHits = cms.int32(-1),
+    maxTrackerNormChi2 = cms.double(-1.0)
+)
