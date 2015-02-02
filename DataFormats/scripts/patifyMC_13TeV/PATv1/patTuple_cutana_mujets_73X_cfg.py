96d845b25d3178ade4a1615b5daf2105f1d4d720
Sven Dildick <sven.dildick@cern.ch>
2015-02-02 12:07:43 -0600

    Create patTuple_cutana_mujets_73X_cfg.py

---
+++ DataFormats/scripts/patifyMC_13TeV/PATv1/patTuple_cutana_mujets_73X_cfg.py
@@ -0,0 +1,40 @@
+## import skeleton process
+from PhysicsTools.PatAlgos.patTemplate_cfg import *
+
+# verbose flags for the PF2PAT modules
+process.options.allowUnscheduled = cms.untracked.bool(True)
+
+### Add MuJet Dataformats
+from MuJetAnalysis.DataFormats.EventContent_version9_cff import *
+process = customizePatOutput(process)
+
+process.load("MuJetAnalysis.DataFormats.RECOtoPAT_cff")
+process.patMuons.addGenMatch = cms.bool(True)
+process.patMuons.embedGenMatch = cms.bool(True)
+
+## pick latest HLT process
+process.patTrigger.processName = cms.string( "*" )
+process.patTriggerEvent.processName = cms.string( "*" )
+
+process.source = cms.Source("PoolSource",
+  fileNames = cms.untracked.vstring(
+        'file:out_hlt.root'
+  )
+)
+
+process.maxEvents.input = -1
+
+process.load("MuJetAnalysis.MuJetProducer.MuJetProducer_cff")
+process.load("MuJetAnalysis.CutFlowAnalyzer.CutFlowAnalyzer_cfi")
+
+process.p = cms.Path(
+    process.TrackerMuJetProducer05 *
+    process.PFMuJetProducer05 *
+    process.cutFlowAnalyzer
+)
+
+process.outpath = cms.EndPath(process.out)
+
+process.TFileService = cms.Service("TFileService",
+    fileName = cms.string("out_ana.root")
+)
