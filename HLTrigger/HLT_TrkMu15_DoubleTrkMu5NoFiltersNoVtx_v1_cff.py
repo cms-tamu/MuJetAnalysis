056dbbc5e6c22c24e484945efe924613f2e5a0f3
Sven Dildick <sven.dildick@cern.ch>
2015-02-03 11:23:02 -0600

    Fix the prescaler

--- HLTrigger/HLT_TrkMu15_DoubleTrkMu5NoFiltersNoVtx_v1_cff.py
+++ HLTrigger/HLT_TrkMu15_DoubleTrkMu5NoFiltersNoVtx_v1_cff.py
@@ -129,6 +129,7 @@ hltSingleTrkMuFiltered15NoVtx = cms.EDFilter("HLTMuonTrkFilter",
 
 def add_HLT_TrkMu15_DoubleTrkMu5NoFiltersNoVtx_v1(process):
 
+    process.hltPreTrkMu15DoubleTrkMu5NoFiltersNoVtx = hltPreTrkMu15DoubleTrkMu5NoFiltersNoVtx
     process.hltL2pfL1sDoubleMu103p5L1f0L2PreFiltered0NoVtx = hltL2pfL1sDoubleMu103p5L1f0L2PreFiltered0NoVtx
     process.hltL2fL1sDoubleMu103p5L1f0L2Filtered10OneMuNoVtx = hltL2fL1sDoubleMu103p5L1f0L2Filtered10OneMuNoVtx
     process.hltL3pfL1sDoubleMu103p5L1f0L2pf0TwoMuL3PreFiltered5NoVtx = hltL3pfL1sDoubleMu103p5L1f0L2pf0TwoMuL3PreFiltered5NoVtx
