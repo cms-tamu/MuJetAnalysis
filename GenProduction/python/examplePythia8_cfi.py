68ac1d86cc7d03282bd3a0c792e346ffccebaffe
Sven Dildick <sven.dildick@cern.ch>
2015-02-11 13:16:12 -0600

    Pythia8 for DarkSUSY

--- GenProduction/python/examplePythia8_cfi.py
+++ GenProduction/python/examplePythia8_cfi.py
@@ -6,22 +6,19 @@ from Configuration.Generator.Pythia8CommonSettings_cfi import *
 from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
 
 generator = cms.EDFilter("Pythia8GeneratorFilter",
-                         pythiaHepMCVerbosity = cms.untracked.bool(False),
-                         maxEventsToPrint = cms.untracked.int32(0),
-                         pythiaPylistVerbosity = cms.untracked.int32(1),
-                         filterEfficiency = cms.untracked.double(1.0),
-                         crossSection = cms.untracked.double(2423.9),
-                         comEnergy = cms.double(7000.0),
-                         PythiaParameters = cms.PSet(pythia8CommonSettingsBlock,
-                                                     pythia8CUEP8M1SettingsBlock,
-                                                     processParameters = cms.vstring(
-                                                         'WeakSingleBoson:ffbar2W = on',
-                                                         '24:onMode = off',
-                                                         '24:onIfAny = 11',
-                                                     ),
-                                                     parameterSets = cms.vstring('pythia8CommonSettings',
-                                                                                 'pythia8CUEP8M1Settings',
-                                                                                 'processParameters',
-                                                                             )
-                                                 )
-                     )
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    maxEventsToPrint = cms.untracked.int32(0),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(1.0),
+    crossSection = cms.untracked.double(2423.9),
+    comEnergy = cms.double(13000.0),
+    PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CUEP8M1SettingsBlock,
+        processParameters = cms.vstring(),
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CUEP8M1Settings',
+                                    'processParameters',
+                                    )
+        )
+)
