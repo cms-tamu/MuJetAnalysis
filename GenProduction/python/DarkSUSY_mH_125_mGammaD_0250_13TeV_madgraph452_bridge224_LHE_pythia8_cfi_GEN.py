177e74867ccfe0fcb5f4239156a99d9cd7a0524f
Alfredo Castaneda <castaned@cern.ch>
2015-02-24 05:35:40 -0600

    CRAB configuration for GEN sample generation

---
+++ GenProduction/python/DarkSUSY_mH_125_mGammaD_0250_13TeV_madgraph452_bridge224_LHE_pythia8_cfi_GEN.py
@@ -0,0 +1,101 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.19 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# with command line options: DarkSUSY_mH_125_mGammaD_0250_13TeV_madgraph452_bridge224_LHE_pythia8_cfi -s GEN --datatier GEN --conditions auto:run2_mc --magField 38T_PostLS1 --eventcontent RECOSIM --evt_type MuJetAnalysis/GenProduction/DarkSUSY_mH_125_mGammaD_0250_13TeV_madgraph452_bridge224_LHE_pythia8_cfi --fileout out_gen.root -n 10 --no_exec
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
+
+process = cms.Process('GEN')
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_PostLS1_cff')
+process.load('Configuration.StandardSequences.Generator_cff')
+process.load('IOMC.EventVertexGenerators.VtxSmearedRealistic8TeVCollision_cfi')
+process.load('GeneratorInterface.Core.genFilterSummary_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(10)
+)
+
+# Input source
+process.source = cms.Source("LHESource",
+    fileNames = cms.untracked.vstring('file:DarkSUSY_mH_125_mGammaD_0250_13TeV-madgraph452_bridge224_events80k.lhe')
+)
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.19 $'),
+    annotation = cms.untracked.string('MuJetAnalysis/GenProduction/DarkSUSY_mH_125_mGammaD_0250_13TeV_madgraph452_bridge224_LHE_pythia8_cfi nevts:10'),
+    name = cms.untracked.string('Applications')
+)
+
+# Output definition
+
+process.RECOSIMoutput = cms.OutputModule("PoolOutputModule",
+    splitLevel = cms.untracked.int32(0),
+    eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
+    outputCommands = process.RECOSIMEventContent.outputCommands,
+    fileName = cms.untracked.string('output.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string(''),
+        dataTier = cms.untracked.string('GEN')
+    ),
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('generation_step')
+    )
+)
+
+# Additional output definition
+
+# Other statements
+process.genstepfilter.triggerConditions=cms.vstring("generation_step")
+from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc', '')
+
+process.generator = cms.EDFilter("Pythia8HadronizerFilter",
+        pythiaPylistVerbosity = cms.untracked.int32(1),
+        filterEfficiency = cms.untracked.double(1.0),
+        pythiaHepMCVerbosity = cms.untracked.bool(False),
+        comEnergy = cms.double(13000.0),
+        crossSection = cms.untracked.double(0.0),
+        maxEventsToPrint = cms.untracked.int32(0),
+        PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CUEP8M1SettingsBlock,
+        processParameters = cms.vstring(),
+        parameterSets = cms.vstring('pythia8CommonSettings', 
+                                    'pythia8CUEP8M1Settings',
+                                    'processParameters')
+        )
+   )
+
+
+
+
+# Path and EndPath definitions
+process.generation_step = cms.Path(process.pgen)
+process.genfiltersummary_step = cms.EndPath(process.genFilterSummary)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.RECOSIMoutput_step = cms.EndPath(process.RECOSIMoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.generation_step,process.genfiltersummary_step,process.endjob_step,process.RECOSIMoutput_step)
+# filter all path with the production filter sequence
+for path in process.paths:
+    getattr(process,path)._seq = process.generator * getattr(process,path)._seq 
+
