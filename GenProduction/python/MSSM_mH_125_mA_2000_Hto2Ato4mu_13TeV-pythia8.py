5cc4c9b2a06e4de94750510617b611764db09163
Yuriy Pakhotin <pakhotin@users.noreply.github.com>
2015-03-01 15:15:31 -0600

    Update MSSM_mH_125_mA_2000_Hto2Ato4mu_13TeV-pythia8.py

--- GenProduction/python/MSSM_mH_125_mA_2000_Hto2Ato4mu_13TeV-pythia8.py
+++ GenProduction/python/MSSM_mH_125_mA_2000_Hto2Ato4mu_13TeV-pythia8.py
@@ -66,30 +66,33 @@ from Configuration.AlCa.GlobalTag import GlobalTag
 process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:startup', '')
 
 process.generator = cms.EDFilter("Pythia8GeneratorFilter",
-	pythiaPylistVerbosity = cms.untracked.int32(0),
+	pythiaPylistVerbosity = cms.untracked.int32(1),
 	filterEfficiency = cms.untracked.double(1.0),
 	pythiaHepMCVerbosity = cms.untracked.bool(False),
 	comEnergy = cms.double(13000.0),
 	crossSection = cms.untracked.double(1.0),
-	maxEventsToPrint = cms.untracked.int32(1),
+	maxEventsToPrint = cms.untracked.int32(5),
 	PythiaParameters = cms.PSet(
 		pythia8CommonSettingsBlock,
 		pythia8CUEP8M1SettingsBlock,
 		pythiaUESettings = cms.vstring(),
 
-		processParameters = cms.vstring( #This section should be entirely in Pythia 8
-			'Higgs:useBSM = on',   # initialize and use the two-Higgs-doublet BSM states
-			'HiggsBSM:all = off',  # switch off all BSM Higgs processes
-			'HiggsBSM:gg2H2 = on', # switch on only gg->H^0(H_2^0) process
-			#'SUSY:all = on', #turn on production of SUSY particles
-			'35:m0 = 125.0', #  mass of H0 in GeV
-			'36:m0 = 2.0',   #  mass of A0 in GeV
+		processParameters = cms.vstring(
+			# This section should be entirely in Pythia 8. See details in
+		        #   - http://home.thep.lu.se/~torbjorn/pythia82html/HiggsProcesses.html
+		        #   - http://home.thep.lu.se/~torbjorn/pythia82html/ParticleDataScheme.html
+			'Higgs:useBSM = on',     # Initialize and use the two-Higgs-doublet BSM states
+			'HiggsBSM:all = off',    # Switch off all BSM Higgs production
+			'HiggsBSM:gg2H2 = on',   # Switch on gg->H^0(H_2^0) scattering via loop contributions primarily from top. Code 1022. 
+			'35:m0 = 125.0',         #  mass in GeV of H0 (PDG ID = 35)
+			'36:m0 = 2.0',           #  mass in GeV of A0 (PDG ID = 36)
 			# decays of H0 (PDG ID = 35)
-			'35:onMode = off',      # Turn off all decay modes 
-			'35:onIfMatch = 36 36', # Allow decays to A0: H0 ->A0A0
-			#decays of the A0 (PDG ID = 36)
-			'36:onMode = off',       # Turn off all decay modes
-			'36:onIfMatch = 13 -13', # Allow decays to muons: A0 ->mu+mu-
+			'35:onMode = off',       # Turn off all H0 decay modes 
+			'35:onIfMatch = 36 36',  # Allow H0 decays to A0: H0 ->A0A0
+			# decays of A0 (PDG ID = 36)
+			'36:onMode = off',       # Turn off all A0 decay modes
+			'36:onIfMatch = 13 -13', # Allow A0 decays to muons: A0 ->mu+mu-
+			# Useful debug printouts
 			'Init:showProcesses = on',        # Print a list of all processes that will be simulated, with their estimated cross section maxima
 			'Init:showChangedSettings = on',  # Print a list of the changed flag/mode/parameter/word setting
 			#'Init:showAllParticleData = on', # Print a list of all particle and decay data. Warning: this will be a long list
