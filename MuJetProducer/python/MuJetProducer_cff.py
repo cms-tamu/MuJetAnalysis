8794cba599fe59b7deef552882b4fdfcda0cb4d1
Sven Dildick <sven.dildick@cern.ch>
2015-04-20 16:06:20 -0500

    Put non-muon jet inclusions in RECOtoPAT_cff

--- MuJetProducer/python/MuJetProducer_cff.py
+++ MuJetProducer/python/MuJetProducer_cff.py
@@ -1,10 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 
 # TransientTrackBuilder is needed for vertexing in MuJetProducer
-from TrackingTools.TransientTrack.TransientTrackBuilder_cfi import *
-from Configuration.Geometry.GeometryIdeal_cff import *
-from Configuration.StandardSequences.MagneticField_cff import *
-from TrackingTools.GeomPropagators.SmartPropagator_cff import *
+#from TrackingTools.TransientTrack.TransientTrackBuilder_cfi import *
+#from Configuration.Geometry.GeometryIdeal_cff import *
+#from Configuration.StandardSequences.MagneticField_cff import *
+#from TrackingTools.GeomPropagators.SmartPropagator_cff import *
 from MuJetAnalysis.MuJetProducer.MuJetProducer_cfi import *
 
 TrackerMuJetProducer05 = MuJetProducer.clone(
