bbdb8c1f5a277f75873522263c2462705a8b0663
Alfredo Castaneda <castaned@cern.ch>
2015-05-29 08:00:23 -0500

    updated

--- GenProduction/python/RunIISpring/create_configs_GENSIM.sh
+++ GenProduction/python/RunIISpring/create_configs_GENSIM.sh
@@ -9,7 +9,7 @@ cd Submit_mass_${1}_ctau_${2}
 
 #=========================      cmsDriver configurations ===========================================//
 
-cmsDriver.py MuJetAnalysis/GenProduction/Pythia8HadronizerFilter_13TeV_cfi --step GEN,SIM --mc --eventcontent RAWSIM --datatier GEN-SIM --conditions MCRUN2_71_V1::All --customise SLHCUpgradeSimulations/Configuration/postLS1Customs.customisePostLS1 --beamspot NominalCollision2015 --magField 38T_PostLS1 --python_filename DarkSUSY_mH_125_mGammaD_$1_13TeV_cT_$2_madgraph452_bridge224_LHE_pythia8_cfi_GEN-SIM.py --filetype LHE --filein file:DarkSUSY_mH_125_mGammaD_$1_13TeV_cT_$2_madgraph452_bridge224_events80k.lhe --fileout file:output.root -n 10 --no_exec
+cmsDriver.py MuJetAnalysis/GenProduction/Pythia8HadronizerFilter_13TeV_cfi --step GEN,SIM --mc --eventcontent RAWSIM --datatier GEN-SIM --conditions MCRUN2_71_V1::All --customise SLHCUpgradeSimulations/Configuration/postLS1Customs.customisePostLS1 --beamspot NominalCollision2015 --magField 38T_PostLS1 --python_filename DarkSUSY_mH_125_mGammaD_$1_13TeV_cT_$2_madgraph452_bridge224_LHE_pythia8_cfi_GEN_SIM.py --filetype LHE --filein file:DarkSUSY_mH_125_mGammaD_$1_13TeV_cT_$2_madgraph452_bridge224_events80k.lhe --fileout file:output.root -n 10 --no_exec
 
 #========================    CRAB submission scripts ====================================================//
 
@@ -34,7 +34,7 @@ se_white_list        = T3_US_TAMU
 
 
 [CMSSW]
-pset        = DarkSUSY_mH_125_mGammaD_${1}_13TeV_cT_${2}_madgraph452_bridge224_LHE_pythia8_cfi_GEN-SIM.py
+pset        = DarkSUSY_mH_125_mGammaD_${1}_13TeV_cT_${2}_madgraph452_bridge224_LHE_pythia8_cfi_GEN_SIM.py
 output_file = output.root
 datasetpath = None
 generator   = lhe
@@ -48,9 +48,9 @@ copy_data               = 1
 return_data             = 0
 storage_element         = T3_US_TAMU
 additional_input_files  = DarkSUSY_mH_125_mGammaD_$1_13TeV_cT_$2_madgraph452_bridge224_events80k.lhe
-user_remote_dir         = DarkSUSY_mH_125_mGammaD_$1_13TeV_cT_$2_madgraph452_bridge224_LHE_pythia8_7115p1_GEN-SIM_v1
-ui_working_dir          = DarkSUSY_mH_125_mGammaD_$1_13TeV_cT_$2_madgraph452_bridge224_LHE_pythia8_7115p1_GEN-SIM_v1
-publish_data_name       = DarkSUSY_mH_125_mGammaD_$1_13TeV_cT_$2_madgraph452_bridge224_LHE_pythia8_7115p1_GEN-SIM_v1
+user_remote_dir         = DarkSUSY_mH_125_mGammaD_$1_13TeV_cT_$2_madgraph452_bridge224_LHE_pythia8_GEN_SIM_7115p1_v1
+ui_working_dir          = DarkSUSY_mH_125_mGammaD_$1_13TeV_cT_$2_madgraph452_bridge224_LHE_pythia8_GEN_SIM_7115p1_v1
+publish_data_name       = DarkSUSY_mH_125_mGammaD_$1_13TeV_cT_$2_madgraph452_bridge224_LHE_pythia8_GEN_SIM_7115p1_v1
 publish_data            = 1
 dbs_url_for_publication = https://cmsdbsprod.cern.ch:8443/cms_dbs_ph_analysis_01_writer/servlet/DBSServlet
 srm_version 		= srmv2
