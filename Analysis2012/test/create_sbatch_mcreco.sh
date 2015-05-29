642c9198017ae62bf26b5734f03703158af44bcc
Alfredo Castaneda <castaned@cern.ch>
2015-05-29 08:06:59 -0500

    updated

--- Analysis2012/test/create_sbatch_mcreco.sh
+++ Analysis2012/test/create_sbatch_mcreco.sh
@@ -35,12 +35,12 @@ cat  >  SBATCH_JOBS/${name}.slrm <<EOF
 #SBATCH -e $PWD/SBATCHLOG/SBATCH_%a.stderr
       
 export PROCESS=\$SLURM_ARRAY_TASK_ID
-cd /fdata/scratch/castaned/Analysis2015data/SL6/CMSSW_7_4_1_patch1/src/
+cd /fdata/scratch/castaned/Analysis2015data/SL6/MuJetAnalysis_TAMU/CMSSW_7_4_1_patch1/src/
 export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
 source $VO_CMS_SW_DIR/cmsset_default.sh
 export SCRAM_ARCH=slc6_amd64_gcc491
 eval \`scramv1 runtime -sh\`
-cd /fdata/scratch/castaned/Analysis2015data/SL6/CMSSW_7_4_1_patch1/src/MuJetAnalysis/Analysis2012/test
+cd /fdata/scratch/castaned/Analysis2015data/SL6/MuJetAnalysis_TAMU/CMSSW_7_4_1_patch1/src/MuJetAnalysis/Analysis2012/test
 cmsRun analysis_mcreco_cfg.py $name $nJobs
       
 exit 0
