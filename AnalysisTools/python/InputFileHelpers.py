b0bfe941b5b02d21e0ced11b867703784493cba4
Sven Dildick <sven.dildick@cern.ch>
2015-05-31 14:58:53 -0500

    add pileup helper function

--- AnalysisTools/python/InputFileHelpers.py
+++ AnalysisTools/python/InputFileHelpers.py
@@ -1,6 +1,23 @@
 import FWCore.ParameterSet.Config as cms
 import os
 
+## helper function for pile-up
+def addPileUp(process, pu = 140, filelist = def_filelist):
+    ff = open(filelist, "r")
+    pu_files = ff.read().split('\n')
+    ff.close()
+    pu_files = filter(lambda x: x.endswith('.root'),  pu_files)
+    
+    process.mix.input = cms.SecSource("PoolSource",
+        nbPileupEvents = cms.PSet(
+            averageNumber = cms.double(pu)
+        ),
+        type = cms.string('poisson'),
+        sequential = cms.untracked.bool(False),
+        fileNames = cms.untracked.vstring(*pu_files)
+    )
+    return process
+
 ## use all ROOT files in a given directory as input
 def useInputDir(process, inputDir, pattern="pat"):
     theInputFiles = []
