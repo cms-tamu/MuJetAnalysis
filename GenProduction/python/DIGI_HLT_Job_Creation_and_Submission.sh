ac0a4e53de4cf9e13a1bfba4c351093d91b6e6ca
bmichlin <bmichlin@rice.edu>
2015-06-23 14:34:26 +0200

    Update DIGI_HLT_Job_Creation_and_Submission.sh

--- GenProduction/python/DIGI_HLT_Job_Creation_and_Submission.sh
+++ GenProduction/python/DIGI_HLT_Job_Creation_and_Submission.sh
@@ -114,7 +114,7 @@ foreach CT $lifetimes {
 	send "sed -i \"s/asdfasdf_to_be_replaced_by_datasetpath/\$(sed 's:\/:\\\\\/:g' /home/$USER/CMSSW_7_1_15_patch1/src/MuJetAnalysis/GEN_SIM/DarkSUSY_mH_125_mGammaD_$mGammaD\_cT_$CT\_13TeV_madgraph452_bridge224_events80k_LHE_pythia8_cfi_GEN_SIM_v1.txt)/\" DIGI_L1_DIGI2RAW_HLT_25ns_mGammaD_$mGammaD\_cT_$CT\_PoissonOOTPU_MCRUN2_74_V9.CRAB.cfg \r";
 	sleep 5;
 	expect "$ ";
-	send "sed -i '/number_of_jobs         = 400/c\\number_of_jobs         = $JOBS' DIGI_L1_DIGI2RAW_HLT_25ns_mGammaD_$mGammaD\_cT_$CT\_PoissonOOTPU_MCRUN2_74_V9.CRAB.cfg \r";
+	send "sed -i '/number_of_jobs           = 400/c\\number_of_jobs         = $JOBS' DIGI_L1_DIGI2RAW_HLT_25ns_mGammaD_$mGammaD\_cT_$CT\_PoissonOOTPU_MCRUN2_74_V9.CRAB.cfg \r";
 	expect "$ ";
 	send "crab -create -cfg DIGI_L1_DIGI2RAW_HLT_25ns_mGammaD_$mGammaD\_cT_$CT\_PoissonOOTPU_MCRUN2_74_V9.CRAB.cfg \r";	
 	sleep 5;
