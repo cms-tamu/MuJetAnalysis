4c20a9219b12928d8d6f3603294ad89cd02086c0
Sven Dildick <sven.dildick@cern.ch>
2016-01-27 10:32:04 -0600

    fix initialization

--- CutFlowAnalyzer/plugins/CutFlowAnalyzer.cc
+++ CutFlowAnalyzer/plugins/CutFlowAnalyzer.cc
@@ -593,10 +593,10 @@ CutFlowAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup
   b_diMuonF_m1_FittedVtx_hitpix=-1000;
   b_diMuonF_m2_FittedVtx_hitpix=-1000;
 
-  b_diMuonC_m1_FittedVtx_hitpix_l3inc=-1000;
-  b_diMuonC_m2_FittedVtx_hitpix_l3inc=-1000;
-  b_diMuonF_m1_FittedVtx_hitpix_l3inc=-1000;
-  b_diMuonF_m2_FittedVtx_hitpix_l3inc=-1000;
+  b_diMuonC_m1_FittedVtx_hitpix_l2inc=-1000;
+  b_diMuonC_m2_FittedVtx_hitpix_l2inc=-1000;
+  b_diMuonF_m1_FittedVtx_hitpix_l2inc=-1000;
+  b_diMuonF_m2_FittedVtx_hitpix_l2inc=-1000;
 
   b_diMuonC_m1_FittedVtx_hitpix_l3inc=-1000;
   b_diMuonC_m2_FittedVtx_hitpix_l3inc=-1000;
