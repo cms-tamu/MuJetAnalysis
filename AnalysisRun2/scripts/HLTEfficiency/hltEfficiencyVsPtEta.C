f9fa6d4efab78b5862567d6daa3f11e66a85cbdf
Sven Dildick <sven.dildick@cern.ch>
2016-04-08 12:38:07 -0500

    update trigger efficiencies

--- AnalysisRun2/scripts/HLTEfficiency/hltEfficiencyVsPtEta.C
+++ AnalysisRun2/scripts/HLTEfficiency/hltEfficiencyVsPtEta.C
@@ -37,19 +37,19 @@ void efficiency_trigger(const std::vector<std::string>& dirNames, int layers = 1
 
   cout << "Preparing histograms" << endl;
   TH1D* leading_pt_fid = new TH1D("leading_pt_fid" + fileName,"",50,0.,50.);
-  TH1D* fullAna_leading_pt_fid = new TH1D("fullAna_leading_pt_fid" + fileName,"",50,0.,50.);
+  TH1D* RECO_leading_pt_fid = new TH1D("RECO_leading_pt_fid" + fileName,"",50,0.,50.);
   TH1D* hlt_leading_pt_fid = new TH1D("hlt_leading_pt_fid" + fileName,"",50,0.,50.);
-  TH1D* hlt_fullAna_leading_pt_fid = new TH1D("hlt_fullAna_leading_pt_fid" + fileName,"",50,0.,50.);
+  TH1D* hlt_RECO_leading_pt_fid = new TH1D("hlt_RECO_leading_pt_fid" + fileName,"",50,0.,50.);
 
   TH1D* leading_eta_fid = new TH1D("leading_eta_fid" + fileName,"",50,-2.5,2.5);
-  TH1D* fullAna_leading_eta_fid = new TH1D("fullAna_leading_eta_fid" + fileName,"",50,-2.5,2.5);
+  TH1D* RECO_leading_eta_fid = new TH1D("RECO_leading_eta_fid" + fileName,"",50,-2.5,2.5);
   TH1D* hlt_leading_eta_fid = new TH1D("hlt_leading_eta_fid" + fileName,"",50,-2.5,2.5);
-  TH1D* hlt_fullAna_leading_eta_fid = new TH1D("hlt_fullAna_leading_eta_fid" + fileName,"",50,-2.5,2.5);
+  TH1D* hlt_RECO_leading_eta_fid = new TH1D("hlt_RECO_leading_eta_fid" + fileName,"",50,-2.5,2.5);
 
   TH1D* leading_phi_fid = new TH1D("leading_phi_fid" + fileName,"",60,-TMath::Pi(),TMath::Pi());
-  TH1D* fullAna_leading_phi_fid = new TH1D("fullAna_leading_phi_fid" + fileName,"",60,-TMath::Pi(),TMath::Pi());
+  TH1D* RECO_leading_phi_fid = new TH1D("RECO_leading_phi_fid" + fileName,"",60,-TMath::Pi(),TMath::Pi());
   TH1D* hlt_leading_phi_fid = new TH1D("hlt_leading_phi_fid" + fileName,"",60,-TMath::Pi(),TMath::Pi());
-  TH1D* hlt_fullAna_leading_phi_fid = new TH1D("hlt_fullAna_leading_phi_fid" + fileName,"",60,-TMath::Pi(),TMath::Pi());
+  TH1D* hlt_RECO_leading_phi_fid = new TH1D("hlt_RECO_leading_phi_fid" + fileName,"",60,-TMath::Pi(),TMath::Pi());
 
   // add files to the chain
   addfilesMany(chain, dirNames, ext);
@@ -78,6 +78,21 @@ void efficiency_trigger(const std::vector<std::string>& dirNames, int layers = 1
   Float_t genMu1_phi;
   Float_t genMu2_phi;
   Float_t genMu3_phi;
+
+  Float_t selMu0_px;
+  Float_t selMu1_px;
+  Float_t selMu2_px;
+  Float_t selMu3_px;
+
+  Float_t selMu0_py;
+  Float_t selMu1_py;
+  Float_t selMu2_py;
+  Float_t selMu3_py;
+
+  Float_t selMu0_eta;
+  Float_t selMu1_eta;
+  Float_t selMu2_eta;
+  Float_t selMu3_eta;
 
   Bool_t is1SelMu17;
   Bool_t is2SelMu8;
@@ -183,6 +198,21 @@ void efficiency_trigger(const std::vector<std::string>& dirNames, int layers = 1
     t->SetBranchAddress("genMu3_phi",    &genMu3_phi);
 
     // RECO Level Selectors
+    t->SetBranchAddress("selMu0_px",    &selMu0_px);
+    t->SetBranchAddress("selMu1_px",    &selMu1_px);
+    t->SetBranchAddress("selMu2_px",    &selMu2_px);
+    t->SetBranchAddress("selMu3_px",    &selMu3_px);
+
+    t->SetBranchAddress("selMu0_py",    &selMu0_py);
+    t->SetBranchAddress("selMu1_py",    &selMu1_py);
+    t->SetBranchAddress("selMu2_py",    &selMu2_py);
+    t->SetBranchAddress("selMu3_py",    &selMu3_py);
+
+    t->SetBranchAddress("selMu0_eta",    &selMu0_eta);
+    t->SetBranchAddress("selMu1_eta",    &selMu1_eta);
+    t->SetBranchAddress("selMu2_eta",    &selMu2_eta);
+    t->SetBranchAddress("selMu3_eta",    &selMu3_eta);
+
     t->SetBranchAddress("is1SelMu17",                     &is1SelMu17);
     t->SetBranchAddress("is2SelMu8",                      &is2SelMu8);
     t->SetBranchAddress("is3SelMu8",                      &is3SelMu8);
@@ -262,40 +292,57 @@ void efficiency_trigger(const std::vector<std::string>& dirNames, int layers = 1
       if(is4SelMu8)  c4recm++;
 
       //  ===========   GEN LEVEL information  ==============//
-      if(is4GenMu8 and 
-	 fabs(genA0_Lxy)< pixelLayerRadius and 
-	 fabs(genA1_Lxy)< pixelLayerRadius and 
-	 fabs(genA0_Lz)<34.5 and 
-	 fabs(genA1_Lz)<34.5){
+      // if(//is4GenMu8 and 
+      // 	 fabs(genA0_Lxy)< pixelLayerRadius and 
+      // 	 fabs(genA1_Lxy)< pixelLayerRadius and 
+      // 	 fabs(genA0_Lz)<34.5 and 
+      // 	 fabs(genA1_Lz)<34.5){
+	
+      // 	leading_pt_fid->Fill(genMu0_pT);
+      // 	leading_eta_fid->Fill(genMu0_eta);
+      // 	leading_phi_fid->Fill(genMu0_phi);
+	
+      // 	if (isDiMuonHLTFired){
+      // 	  hlt_leading_pt_fid->Fill(genMu0_pT);
+      // 	  hlt_leading_eta_fid->Fill(genMu0_eta);
+      // 	  hlt_leading_phi_fid->Fill(genMu0_phi);
+      // 	}
+      // }
+      
+      // check for 4 reco muons 
+      Int_t nMu = 0;
+      if (selMu0_px!=-100 and selMu0_py!=-100) nMu++;
+      if (selMu1_px!=-100 and selMu1_py!=-100) nMu++;
+      if (selMu2_px!=-100 and selMu2_py!=-100) nMu++;
+      if (selMu3_px!=-100 and selMu3_py!=-100) nMu++;
+      
+      if (nMu>=4) {
+	Float_t selMu0_pT = TMath::Sqrt(selMu0_px*selMu0_px + selMu0_py*selMu0_py);
+	Float_t selMu1_pT = TMath::Sqrt(selMu1_px*selMu1_px + selMu1_py*selMu1_py);
+	Float_t selMu2_pT = TMath::Sqrt(selMu2_px*selMu2_px + selMu2_py*selMu2_py);
+	Float_t selMu3_pT = TMath::Sqrt(selMu3_px*selMu3_px + selMu3_py*selMu3_py);
+	
+	Int_t nMuPt8 = 0;
+	Int_t nMuPt17 = 0;
+	
+	if (selMu0_pT>=8) nMuPt8++;
+	if (selMu1_pT>=8) nMuPt8++;
+	if (selMu2_pT>=8) nMuPt8++;
+	if (selMu3_pT>=8) nMuPt8++;
+	
+	if (selMu0_pT>=17) nMuPt17++;
+	if (selMu1_pT>=17) nMuPt17++;
+	if (selMu2_pT>=17) nMuPt17++;
+	if (selMu3_pT>=17) nMuPt17++;
 
-	leading_pt_fid->Fill(genMu0_pT);
-	leading_eta_fid->Fill(genMu0_eta);
-	leading_phi_fid->Fill(genMu0_phi);
-	
-	if (isDiMuonHLTFired){
-	  hlt_leading_pt_fid->Fill(genMu0_pT);
-	  hlt_leading_eta_fid->Fill(genMu0_eta);
-	  hlt_leading_phi_fid->Fill(genMu0_phi);
-	}
-	
-	if(is4SelMu8 and 
-	   isVertexOK and 
-	   is2MuJets and 
-	   is2DiMuons and 
-	   is2DiMuonsFittedVtxOK and 
-	   pixelLayer and 
-	   is2DiMuonsDzOK_FittedVtx and 
-	   is2DiMuonsMassOK_FittedVtx and 
-	   is2DiMuonsIsoTkOK_FittedVtx){
-	  
-	  fullAna_leading_pt_fid->Fill(genMu0_pT);
-	  fullAna_leading_eta_fid->Fill(genMu0_eta);
-	  fullAna_leading_phi_fid->Fill(genMu0_phi);
-	  
+	if (nMuPt8>=4 and nMuPt17>=1){
+	  RECO_leading_pt_fid->Fill(selMu0_pT);
+	  RECO_leading_eta_fid->Fill(selMu0_eta);
+	  //	RECO_leading_phi_fid->Fill(selMu0_phi);
 	  if (isDiMuonHLTFired){
-	    hlt_fullAna_leading_pt_fid->Fill(genMu0_pT);
-	    hlt_fullAna_leading_eta_fid->Fill(genMu0_eta);
-	    hlt_fullAna_leading_phi_fid->Fill(genMu0_phi);
+	    hlt_RECO_leading_pt_fid->Fill(selMu0_pT);
+	    hlt_RECO_leading_eta_fid->Fill(selMu0_eta);
+	    //	  hlt_RECO_leading_phi_fid->Fill(selMu0_phi);
 	  }
 	}
       }
@@ -307,9 +354,9 @@ void efficiency_trigger(const std::vector<std::string>& dirNames, int layers = 1
   TEfficiency* eff_hlt_leading_pt_fid = new TEfficiency(*hlt_leading_pt_fid, *leading_pt_fid);
   TEfficiency* eff_hlt_leading_eta_fid = new TEfficiency(*hlt_leading_eta_fid, *leading_eta_fid);
   TEfficiency* eff_hlt_leading_phi_fid = new TEfficiency(*hlt_leading_phi_fid, *leading_phi_fid);
-  TEfficiency* eff_hlt_fullAna_leading_pt_fid = new TEfficiency(*hlt_fullAna_leading_pt_fid, *fullAna_leading_pt_fid);
-  TEfficiency* eff_hlt_fullAna_leading_eta_fid = new TEfficiency(*hlt_fullAna_leading_eta_fid, *fullAna_leading_eta_fid);
-  TEfficiency* eff_hlt_fullAna_leading_phi_fid = new TEfficiency(*hlt_fullAna_leading_phi_fid, *fullAna_leading_phi_fid);
+  TEfficiency* eff_hlt_RECO_leading_pt_fid = new TEfficiency(*hlt_RECO_leading_pt_fid, *RECO_leading_pt_fid);
+  TEfficiency* eff_hlt_RECO_leading_eta_fid = new TEfficiency(*hlt_RECO_leading_eta_fid, *RECO_leading_eta_fid);
+  TEfficiency* eff_hlt_RECO_leading_phi_fid = new TEfficiency(*hlt_RECO_leading_phi_fid, *RECO_leading_phi_fid);
   
   enum Variable{Pt=0,Eta=1,Phi=3};
 
@@ -325,7 +372,7 @@ void efficiency_trigger(const std::vector<std::string>& dirNames, int layers = 1
       double xMax;
       int xBin = 50;
       TString xTitle;
-      TString yTitle = "Trigger efficiency";
+      TString yTitle = "HLT_TrkMu15_DoubleTrkMu5NoFiltersNoVtx efficiency";
       TString title;
 
       if (v==Variable::Pt){
@@ -372,12 +419,12 @@ void efficiency_trigger(const std::vector<std::string>& dirNames, int layers = 1
     }
   };
 
-  MyEfficiencyPlot::plot(Variable::Pt,  eff_hlt_leading_pt_fid, "eff_hlt_vs_leading_pT_L" + std::to_string(layers));
-  MyEfficiencyPlot::plot(Variable::Eta, eff_hlt_leading_eta_fid, "eff_hlt_vs_leading_eta_L" + std::to_string(layers));
-  MyEfficiencyPlot::plot(Variable::Phi, eff_hlt_leading_phi_fid, "eff_hlt_vs_leading_phi_L" + std::to_string(layers));
-  MyEfficiencyPlot::plot(Variable::Pt,  eff_hlt_fullAna_leading_pt_fid, "eff_hlt_fullAna_vs_leading_pT_L" + std::to_string(layers));
-  MyEfficiencyPlot::plot(Variable::Eta, eff_hlt_fullAna_leading_eta_fid, "eff_hlt_fullAna_vs_leading_eta_L" + std::to_string(layers));
-  MyEfficiencyPlot::plot(Variable::Phi, eff_hlt_fullAna_leading_phi_fid, "eff_hlt_fullAna_vs_leading_phi_L" + std::to_string(layers));
+  // MyEfficiencyPlot::plot(Variable::Pt,  eff_hlt_leading_pt_fid, "eff_hlt_vs_leading_pT_L" + std::to_string(layers));
+  // MyEfficiencyPlot::plot(Variable::Eta, eff_hlt_leading_eta_fid, "eff_hlt_vs_leading_eta_L" + std::to_string(layers));
+  // MyEfficiencyPlot::plot(Variable::Phi, eff_hlt_leading_phi_fid, "eff_hlt_vs_leading_phi_L" + std::to_string(layers));
+  // MyEfficiencyPlot::plot(Variable::Pt,  eff_hlt_RECO_leading_pt_fid, "eff_hlt_RECO_vs_leading_pT_L" + std::to_string(layers));
+  MyEfficiencyPlot::plot(Variable::Eta, eff_hlt_RECO_leading_eta_fid, "eff_hlt_RECO_vs_leading_eta");
+  // MyEfficiencyPlot::plot(Variable::Phi, eff_hlt_RECO_leading_phi_fid, "eff_hlt_RECO_vs_leading_phi_L" + std::to_string(layers));
 }
 
 void hltEfficiencyVsPtEta()
@@ -386,8 +433,8 @@ void hltEfficiencyVsPtEta()
   readTextFileWithSamples("ANASamplesSven2.txt", DarkSUSY_mH_125_mGammaD_v);
   // printFileNames(DarkSUSY_mH_125_mGammaD_v);
 
-  for(auto v: DarkSUSY_mH_125_mGammaD_v) efficiency_trigger(v, 1);
-  for(auto v: DarkSUSY_mH_125_mGammaD_v) efficiency_trigger(v, 2);
-  for(auto v: DarkSUSY_mH_125_mGammaD_v) efficiency_trigger(v, 3);
+  for(auto v: DarkSUSY_mH_125_mGammaD_v) efficiency_trigger(v);
+  // for(auto v: DarkSUSY_mH_125_mGammaD_v) efficiency_trigger(v, 2);
+  // for(auto v: DarkSUSY_mH_125_mGammaD_v) efficiency_trigger(v, 3);
 }
 
