e975dea8700f2188f2eed7eab23a9b8af1fb9461
Luca <luca.pernie@cern.ch>
2016-05-26 20:45:01 +0200

    working version of curflow

--- DataFormats/scripts/patifyData_Run2015_bbEstimation/crab_eos_2015Dv1Dec.py
+++ DataFormats/scripts/patifyData_Run2015_bbEstimation/crab_eos_2015Dv1Dec.py
@@ -2,7 +2,7 @@ from CRABClient.UserUtilities import config
 config = config()
 
 config.section_("General")
-config.General.requestName = 'DoubleMuon_2015Dv1noPrompt_AOD_260373_260534_7412p4_p1_patTuple_cutana_mujets_03'
+config.General.requestName = 'DoubleMuon_2015Dv1noPrompt_AOD_260373_260534_7412p4_p1_patTuple_cutana_mujets_14'
 config.General.workArea = 'crab_projects'
 config.General.transferLogs = True
 
@@ -18,13 +18,15 @@ config.Data.splitting = 'LumiBased'
 config.Data.unitsPerJob = 5
 config.Data.lumiMask = 'Cert_246908-260627_13TeV_PromptReco_Collisions15_25ns_JSON_MuonPhys_v2.txt'
 #config.Data.outLFNDirBase = '/store/group/alca_muonalign/lpernie/DoubleMuonRun2015D_PromptReco_AOD246908-258750_7412p4_patch1_patTuple_cutana_mujets'
-config.Data.outLFNDirBase = '/store/group/lpcdarksusy/'
+#config.Data.outLFNDirBase = '/store/group/lpcdarksusy/'
+config.Data.outLFNDirBase = '/store/user/lpernie/'
 config.Data.publication = True
-config.Data.outputDatasetTag = 'DoubleMuon_2015Dv1noPrompt_AOD_260373_260534_7412p4_p1_patTuple_cutana_mujets_03'
+config.Data.outputDatasetTag = 'DoubleMuon_2015Dv1noPrompt_AOD_260373_260534_7412p4_p1_patTuple_cutana_mujets_14'
 
 config.section_("Site")
 #config.Site.storageSite = 'T2_CH_CERN'
-config.Site.storageSite = 'T3_US_FNALLPC'
+#config.Site.storageSite = 'T3_US_FNALLPC'
+config.Site.storageSite = 'T3_US_TAMU'
 
 #https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/
 #https://cmsweb.cern.ch/das/request?view=list&limit=50&instance=prod%2Fglobal&input=dataset%3D%2FDoubleMu*%2FRun2015*-PromptReco-v1%2FAOD
--- DataFormats/scripts/patifyData_Run2015_bbEstimation/patTuple_cutana_mujets_73X_cfg.py
+++ DataFormats/scripts/patifyData_Run2015_bbEstimation/patTuple_cutana_mujets_73X_cfg.py
@@ -1,20 +1,56 @@
-## import skeleton process
+import FWCore.ParameterSet.Config as cms
+import sys
+import os
+
 from PhysicsTools.PatAlgos.patTemplate_cfg import *
-
-from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
-###process.GlobalTag = GlobalTag(process.GlobalTag, '74X_dataRun2_Prompt_v2')
-process.GlobalTag = GlobalTag(process.GlobalTag, '76X_dataRun2_v15')
-
-# verbose flags for the PF2PAT modules
-process.options.allowUnscheduled = cms.untracked.bool(True)
-
-### Add MuJet Dataformats
 from MuJetAnalysis.DataFormats.EventContent_version10_cff import *
 process = customizePatOutput(process)
 
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_PostLS1_cff')
+process.load('Configuration.StandardSequences.Reconstruction_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+
+process.options = cms.untracked.PSet(
+    allowUnscheduled = cms.untracked.bool(True)
+)
+
+# Other statements
+from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc', '')
+
+### Track refitter specific stuff
+from RecoTracker.TkNavigation.NavigationSchoolESProducer_cff import *
+process.load("RecoTracker.TrackProducer.TrackRefitter_cfi")
+process.TrackRefitter.NavigationSchool = ''
+
+################## RECO Input #############################
+
+process.source = cms.Source ("PoolSource",
+                             fileNames=cms.untracked.vstring(
+							 'file:002C811B-80A7-E511-8C4D-0CC47A4D7644.root'
+							 ),
+                             skipEvents=cms.untracked.uint32(0)
+                            )
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(10))
+
+
+process.Path = cms.Path(process.TrackRefitter*process.MeasurementTrackerEvent)
+
+# End of customisation functions
+#do not add changes to your config after this point (unless you know what you are doing)
+from FWCore.ParameterSet.Utilities import convertToUnscheduled
+process=convertToUnscheduled(process)
+
+################# RECOtoPAT  ==========================================
 process.load("MuJetAnalysis.DataFormats.RECOtoPAT_cff")
-#process.patMuons.addGenMatch = cms.bool(True)
-#process.patMuons.embedGenMatch = cms.bool(True)
 process.patMuons.addGenMatch = cms.bool(False)
 process.patMuons.embedGenMatch = cms.bool(False)
 
@@ -22,25 +58,14 @@ process.patMuons.embedGenMatch = cms.bool(False)
 process.patTrigger.processName = cms.string( "*" )
 process.patTriggerEvent.processName = cms.string( "*" )
 
+############## Analysis Modules ###################################
 process.load("MuJetAnalysis.MuJetProducer.MuJetProducer_cff")
-process.load("MuJetAnalysis.CutFlowAnalyzer.CutFlowAnalyzer_cfi")
-process.cutFlowAnalyzer.fillGenLevel = cms.bool(False)
+process.load("MuJetAnalysis.CutFlowAnalyzer.CutFlowAnalyzer_cff")
+#process.Path = cms.Path(process.patifyMC * process.MuJetProducers  * process.cutFlowAnalyzers)
+#process.Path = cms.Path(process.patifyData * process.MuJetProducers  * process.cutFlowAnalyzers)
+process.Path = cms.Path(process.patifyData * process.MuJetProducers  * process.cutFlowAnalyzers)
+# customisation of the process.
 
-process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring(
-		"file:002DAE91-77A7-E511-B61B-00266CFAEA48.root"
-    )
-)
+process.TFileService = cms.Service("TFileService", fileName = cms.string("out_ana.root") )
 
-process.maxEvents.input = -1
-
-process.p = cms.Path(
-	process.MuJetProducers * 
-    process.cutFlowAnalyzer
-)
-
-process.outpath = cms.EndPath(process.out)
-
-process.TFileService = cms.Service("TFileService",
-    fileName = cms.string("out_ana.root")
-)
+# End of customisation functions
