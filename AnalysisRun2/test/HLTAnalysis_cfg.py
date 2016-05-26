ab2c1369ca9e09eccffb62e8f20f49144f8183c2
Sven Dildick <sven.dildick@cern.ch>
2016-05-27 00:37:03 +0200

    update HLT config

--- AnalysisRun2/test/HLTAnalysis_cfg.py
+++ AnalysisRun2/test/HLTAnalysis_cfg.py
@@ -1,14 +1,18 @@
 import FWCore.ParameterSet.Config as cms
+
 from Configuration.StandardSequences.Eras import eras
 
 process = cms.Process("ANALYSIS", eras.Run2_2016)
-
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load("FWCore.MessageService.MessageLogger_cfi")
 process.load('Configuration.Geometry.GeometryExtended2016_cff')
 process.load('Configuration.Geometry.GeometryExtended2016Reco_cff')
-process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
-from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
-process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_data')
 
 process.options = cms.untracked.PSet(
     wantSummary = cms.untracked.bool(True)
@@ -19,7 +23,6 @@ process.source = cms.Source("PoolSource",
         'file:B6687419-D919-E611-9A7C-02163E014647.root'
     )
 )
-
 
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32( 100 )
@@ -41,5 +44,7 @@ process.hltanalysis.l1GtReadoutRecord               = cms.InputTag("")
 
 #process.analyzeThis = cms.Path( process.HLTBeginSequence + process.hltanalysis )
 process.analyzeThis = cms.Path(process.hltanalysis )
-
 process.schedule = cms.Schedule(process.analyzeThis)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+
+#process.FEVTDEBUGHLToutput_step = cms.EndPath(process.FEVTDEBUGHLToutput)
