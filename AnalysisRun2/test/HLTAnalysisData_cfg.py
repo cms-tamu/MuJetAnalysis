919cb04e20adfd53c377d2305b41d9b120a262f6
Sven Dildick <sven.dildick@cern.ch>
2016-05-27 00:41:48 +0200

    only 1 config for HLT

--- AnalysisRun2/test/HLTAnalysisData_cfg.py
+++ AnalysisRun2/test/HLTAnalysisData_cfg.py
@@ -1,14 +1,17 @@
 import FWCore.ParameterSet.Config as cms
+
 from Configuration.StandardSequences.Eras import eras
 
 process = cms.Process("ANALYSIS", eras.Run2_2016)
-
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load("FWCore.MessageService.MessageLogger_cfi")
-process.load('Configuration.Geometry.GeometryExtended2016_cff')
-process.load('Configuration.Geometry.GeometryExtended2016Reco_cff')
-process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
-from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
-process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_data')
 
 process.options = cms.untracked.PSet(
     wantSummary = cms.untracked.bool(True)
@@ -20,7 +23,6 @@ process.source = cms.Source("PoolSource",
     )
 )
 
-
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32( 100 )
 )
@@ -30,6 +32,8 @@ process.load('Configuration/StandardSequences/SimL1Emulator_cff')
 process.load("HLTrigger.HLTanalyzers.HLTAnalyser_cfi")
 #process.hltanalysis.l1GtObjectMapRecord             = cms.InputTag("")
 process.hltanalysis.l1GtReadoutRecord               = cms.InputTag("gtDigis::HLT")
+process.hltanalysis.EERecHits                   = cms.InputTag("ecalRecHit","EcalRecHitsEE")
+process.hltanalysis.EBRecHits                   = cms.InputTag("ecalRecHit","EcalRecHitsEB")
 """
 process.hltanalysis.l1GctHFBitCounts                = cms.InputTag("")
 process.hltanalysis.l1GctHFRingSums                 = cms.InputTag("")
@@ -39,5 +43,7 @@ process.hltanalysis.l1GtReadoutRecord               = cms.InputTag("")
 
 #process.analyzeThis = cms.Path( process.HLTBeginSequence + process.hltanalysis )
 process.analyzeThis = cms.Path(process.hltanalysis )
-
 process.schedule = cms.Schedule(process.analyzeThis)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+
+#process.FEVTDEBUGHLToutput_step = cms.EndPath(process.FEVTDEBUGHLToutput)
