156717cca4d355dd992d1cf5a0dc345d625f8b05
Sven Dildick <sven.dildick@cern.ch>
2016-05-27 20:49:40 +0200

    configs for running on Data

--- CutFlowAnalyzer/python/CutFlowAnalyzer_cff.py
+++ CutFlowAnalyzer/python/CutFlowAnalyzer_cff.py
@@ -20,3 +20,24 @@ cutFlowAnalyzers = cms.Sequence(
     cutFlowAnalyzerPXBL3PXFL2
 )
 
+cutFlowAnalyzer_Data = cutFlowAnalyzer.clone(
+    fillGenLevel = cms.bool(False),
+    runBBestimation = cms.untracked.bool(True),
+)
+cutFlowAnalyzerPXBL2PXFL2_Data = cutFlowAnalyzer_Data.clone(
+    barrelPixelLayer = cms.int32(2),
+    endcapPixelLayer = cms.int32(2),
+    muJets = cms.InputTag("PFMuJetProducer05PXBL2PXFL2"),
+    muJetOrphans = cms.InputTag("PFMuJetProducer05PXBL2PXFL2", "Orphans"),
+)
+cutFlowAnalyzerPXBL3PXFL2_Data = cutFlowAnalyzer_Data.clone(
+    barrelPixelLayer = cms.int32(3),
+    endcapPixelLayer = cms.int32(2),
+    muJets = cms.InputTag("PFMuJetProducer05PXBL3PXFL2"),
+    muJetOrphans = cms.InputTag("PFMuJetProducer05PXBL3PXFL2", "Orphans"),
+)
+cutFlowAnalyzers_Data = cms.Sequence(
+    cutFlowAnalyzer_Data *
+    cutFlowAnalyzerPXBL2PXFL2_Data *
+    cutFlowAnalyzerPXBL3PXFL2_Data
+)
