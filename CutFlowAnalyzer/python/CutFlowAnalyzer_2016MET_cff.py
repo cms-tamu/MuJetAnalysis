cf4f383284c2e0fdc0c2b353a9be2c08996010c5
Sven Dildick <sven.dildick@cern.ch>
2017-03-31 14:42:04 -0500

    add missing files

---
+++ CutFlowAnalyzer/python/CutFlowAnalyzer_2016MET_cff.py
@@ -0,0 +1,44 @@
+import FWCore.ParameterSet.Config as cms
+
+from MuJetAnalysis.CutFlowAnalyzer.CutFlowAnalyzer_2016MET_cfi import *
+
+## Monte Carlo
+cutFlowAnalyzerPXBL2PXFL2 = cutFlowAnalyzer.clone(
+    barrelPixelLayer = cms.int32(2),
+    endcapPixelLayer = cms.int32(2),
+    muJets = cms.InputTag("PFMuJetProducer05PXBL2PXFL2"),
+    muJetOrphans = cms.InputTag("PFMuJetProducer05PXBL2PXFL2", "Orphans"),
+)
+cutFlowAnalyzerPXBL3PXFL2 = cutFlowAnalyzer.clone(
+    barrelPixelLayer = cms.int32(3),
+    endcapPixelLayer = cms.int32(2),
+    muJets = cms.InputTag("PFMuJetProducer05PXBL3PXFL2"),
+    muJetOrphans = cms.InputTag("PFMuJetProducer05PXBL3PXFL2", "Orphans"),
+)
+cutFlowAnalyzers = cms.Sequence(
+    cutFlowAnalyzer * 
+    cutFlowAnalyzerPXBL2PXFL2 * 
+    cutFlowAnalyzerPXBL3PXFL2
+)
+
+## Data
+cutFlowAnalyzer_Data = cutFlowAnalyzer.clone(
+    fillGenLevel = cms.bool(False)
+)
+cutFlowAnalyzerPXBL2PXFL2_Data = cutFlowAnalyzer_Data.clone(
+    barrelPixelLayer = cms.int32(2),
+    endcapPixelLayer = cms.int32(2),
+    muJets = cms.InputTag("PFMuJetProducer05PXBL2PXFL2"),
+    muJetOrphans = cms.InputTag("PFMuJetProducer05PXBL2PXFL2", "Orphans"),
+)
+cutFlowAnalyzerPXBL3PXFL2_Data = cutFlowAnalyzer_Data.clone(
+    barrelPixelLayer = cms.int32(3),
+    endcapPixelLayer = cms.int32(2),
+    muJets = cms.InputTag("PFMuJetProducer05PXBL3PXFL2"),
+    muJetOrphans = cms.InputTag("PFMuJetProducer05PXBL3PXFL2", "Orphans"),
+)
+cutFlowAnalyzers_Data = cms.Sequence(
+    cutFlowAnalyzer_Data *
+    cutFlowAnalyzerPXBL2PXFL2_Data *
+    cutFlowAnalyzerPXBL3PXFL2_Data
+)
