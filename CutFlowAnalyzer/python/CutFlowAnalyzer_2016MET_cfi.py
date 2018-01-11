c64139d23fccaf04b57f166681477e0b27ede56c
Sven Dildick <sven.dildick@cern.ch>
2018-01-11 14:41:34 -0600

    update fill gen level information

--- CutFlowAnalyzer/python/CutFlowAnalyzer_2016MET_cfi.py
+++ CutFlowAnalyzer/python/CutFlowAnalyzer_2016MET_cfi.py
@@ -2,7 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 cutFlowAnalyzer = cms.EDAnalyzer('CutFlowAnalyzer_AOD',
     analyzerDebug = cms.int32(0),
-    fillGenLevel = cms.bool(True),
 #    muons = cms.InputTag("cleanPatTrackerMuonsTriggerMatch"),
 #    muJets = cms.InputTag("TrackerMuJetProducer05"),
     muons = cms.InputTag("cleanPatPFMuonsTriggerMatch"),
