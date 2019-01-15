493080bda56ed1974518ffa2747ae197c1957f7e
Wei Shi <weishi@rice.edu>
2019-01-15 16:38:46 -0600

    Update Helpers.h

--- AnalysisTools/interface/Helpers.h
+++ AnalysisTools/interface/Helpers.h
@@ -18,7 +18,7 @@ double cotan(double i);
 
 bool PtOrder (const reco::GenParticle* p1, const reco::GenParticle* p2);
 
-bool PtOrderPFMu (const pat::Muon* p1, const pat::Muon* p2);
+bool PtOrderPFMu (const reco::Muon* p1, const reco::Muon* p2);
   
 bool sameTrack(const reco::Track *one, const reco::Track *two);
 
