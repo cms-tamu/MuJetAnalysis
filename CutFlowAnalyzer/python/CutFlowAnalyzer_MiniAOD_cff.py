967d9af2efebd5fc9dcebba2e636afb63ae4523a
Wei Shi <weishi@rice.edu>
2019-04-04 15:04:45 -0500

    remove repeat modules

--- CutFlowAnalyzer/python/CutFlowAnalyzer_MiniAOD_cff.py
+++ CutFlowAnalyzer/python/CutFlowAnalyzer_MiniAOD_cff.py
@@ -1,15 +1,5 @@
 import FWCore.ParameterSet.Config as cms
-
 from MuJetAnalysis.CutFlowAnalyzer.CutFlowAnalyzer_MiniAOD_cfi import *
-
-#Used in Phase-0 Pixel: cutFlowAnalyzerPXBL3PXFL2
-#cutFlowAnalyzerPXBL3PXFL2 = cutFlowAnalyzer.clone(
-#    barrelPixelLayer = cms.int32(3),
-#    endcapPixelLayer = cms.int32(2),
-#    muPairs = cms.InputTag("PFMuJetProducer05PXBL3PXFL2", "Pairs"),#All possible Mu pairs
-#    muJets = cms.InputTag("PFMuJetProducer05PXBL3PXFL2"),#Delivered for signal
-#    muJetOrphans = cms.InputTag("PFMuJetProducer05PXBL3PXFL2", "Orphans"),#Delivered to analyzer for bb est
-#)
 
 cutFlowAnalyzerPXBL4PXFL3 = cutFlowAnalyzer.clone(
     barrelPixelLayer = cms.int32(4),
@@ -19,7 +9,5 @@ cutFlowAnalyzerPXBL4PXFL3 = cutFlowAnalyzer.clone(
     muJetOrphans = cms.InputTag("PFMuJetProducer05PXBL4PXFL3", "Orphans"),
 )
 cutFlowAnalyzers = cms.Sequence(
-    #cutFlowAnalyzerPXBL3PXFL2
-    #cutFlowAnalyzerPXBL3PXFL2 *
     cutFlowAnalyzerPXBL4PXFL3
 )
--- MuJetProducer/python/MuJetProducerRun2_cff.py
+++ MuJetProducer/python/MuJetProducerRun2_cff.py
@@ -1,32 +1,18 @@
 import FWCore.ParameterSet.Config as cms
-
 from MuJetAnalysis.MuJetProducer.MuJetProducerRun2_cfi import *
 
-PFMuJetProducer05 = MuJetProducer.clone(
+PFMuJetProducer05PXBL4PXFL3 = MuJetProducer.clone(
     maxMass = cms.double(60.),
     muons = cms.InputTag("cleanPatPFMuonsTriggerMatch"),
     selectTrackerMuons = cms.bool(False),
     selectGlobalMuons = cms.bool(False),
-    groupingMode = cms.string("GroupByMassAndVertexProbOrDeltaR"),
-    maxDeltaR = cms.double(0.01),
     minSegmentMatches = cms.int32(-1),
     minTrackerHits = cms.int32(-1),
-    maxTrackerNormChi2 = cms.double(-1.0)
-)
-
-#Used in Phase-0 Pixel: PFMuJetProducer05PXBL3PXFL2
-#PFMuJetProducer05PXBL3PXFL2 = PFMuJetProducer05.clone(
-#    barrelPixelLayer = cms.int32(3),
-#    endcapPixelLayer = cms.int32(2),
-#)
-
-PFMuJetProducer05PXBL4PXFL3 = PFMuJetProducer05.clone(
+    maxTrackerNormChi2 = cms.double(-1.0),
     barrelPixelLayer = cms.int32(4),
-    endcapPixelLayer = cms.int32(3),
+    endcapPixelLayer = cms.int32(3)
 )
 
 MuJetProducers = cms.Sequence(
-    PFMuJetProducer05 *
-    #PFMuJetProducer05PXBL3PXFL2 *
     PFMuJetProducer05PXBL4PXFL3
 )
