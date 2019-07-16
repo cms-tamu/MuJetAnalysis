cf7553979493352c423e39ce496d843e2fc7e6c4
Wei Shi <weishi@rice.edu>
2019-07-16 18:48:21 -0500

    move mass cut eq. to macro

--- CutFlowAnalyzer/scripts/cutflow_macros/foo_modified.C
+++ CutFlowAnalyzer/scripts/cutflow_macros/foo_modified.C
@@ -428,7 +428,11 @@ void efficiency(const std::vector<std::string>& dirNames)
                   if( is2DiMuonHLTFired ) {
                     counter[k][15]++;
 
-                    if( is2DiMuonsMassOK ){
+                    //if( is2DiMuonsMassOK){//this is an equivalent conditon to the one below
+                    if(
+                      is2DiMuonsFittedVtxOK &&
+                      fabs(massC-massF) < 3*(0.003044 + 0.007025*(massC+massF)/2.0 + 0.000053*(massC+massF)*(massC+massF)/4.0)
+                    ){
                       counter[k][16]++;
                       //==============================================
                       // All offline analysis selections finished
@@ -640,7 +644,7 @@ void efficiency(const std::vector<std::string>& dirNames)
      BKGShapeCRmassFScaled->Write();
    } //end if ( ModelBKGShape )
 
-   if ( ModelSRWidth ) {
+   if ( ModelSRWidth && DimuMass->Integral() > 0 ) {
      DimuMass->SetLineColor(kBlue);
      DimuMass->SetLineWidth(2);
      DimuMass->GetXaxis()->SetTitle("#frac{m_{#mu#mu1}+m_{#mu#mu2}}{2} [GeV]");
