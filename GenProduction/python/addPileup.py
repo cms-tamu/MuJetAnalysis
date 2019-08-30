ad3fdb96fdfb23be29e1ec6df8bc04bfd570785a
Sven Dildick <svendildick@svens-mbp.dyndns.rice.edu>
2019-08-30 16:56:59 -0500

    Remove old MC files

--- GenProduction/python/addPileup.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-from MuJetAnalysis.GenProduction.fileNamesPU import fileNamesPU
-
-def addPileup(process):
-    process.mix.input.fileNames = fileNamesPU
-    return process
-
