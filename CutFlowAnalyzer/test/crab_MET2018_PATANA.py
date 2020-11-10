c7f77aae14d3ef74fb5aa6802b81c5b1158b4a58
Sven Dildick <sven.dildick@cern.ch>
2020-11-10 12:36:03 -0600

    Fix era

--- CutFlowAnalyzer/test/crab_MET2018_PATANA.py
+++ CutFlowAnalyzer/test/crab_MET2018_PATANA.py
@@ -13,80 +13,80 @@ config.JobType.allowUndistributedCMSSW = True
 config.Data.unitsPerJob = 10
 
 cmsRunConfig = '/uscms/home/dildick/nobackup/work/DisplacedMuonJetAnalysis_Run2/CMSSW_10_2_18/src/MuJetAnalysis/DataFormats/scripts/patifyMC_13TeV/patTuple_cutana_mujets_MiniAOD_cfg.py'
-cmsRunArgData2018ABC = " mc=False year=2018 era=ABC"
-cmsRunArgData2018D = " mc=False year=2018 era=ABC"
-cmsRunArgMC2018 = " mc=True year=2018"
+cmsRunArgData2018ABC = "mc=False year=2018 era=ABC"
+cmsRunArgData2018D = "mc=False year=2018 era=D"
+cmsRunArgMC2018 = "mc=True year=2018"
 
 MET2018A = [
     "MET2018A",
     '/MET/dildick-crab_METRun2018A-17Sep2018-v1_Filter20201027_V2-463ea116673913e009b1f501c2d5b5ef/USER',
-    cmsRunConfig + cmsRunArgData2018ABC
+    cmsRunArgData2018ABC
 ]
 
 MET2018B = [
     "MET2018B",
     '/MET/dildick-crab_METRun2018B-17Sep2018-v1_Filter20201027_V2-463ea116673913e009b1f501c2d5b5ef/USER',
-    cmsRunConfig + cmsRunArgData2018ABC
+    cmsRunArgData2018ABC
 ]
 
 MET2018C = [
     'MET2018C',
     '/MET/dildick-crab_METRun2018C-17Sep2018-v1_Filter20201027_V2-463ea116673913e009b1f501c2d5b5ef/USER',
-    cmsRunConfig + cmsRunArgData2018ABC
+    cmsRunArgData2018ABC
 ]
 
 MET2018D = [
     'MET2018D',
     '/MET/dildick-crab_METRun2018D-PromptReco-v2_Filter20201027_V3-463ea116673913e009b1f501c2d5b5ef/USER',
-    cmsRunConfig + cmsRunArgData2018D
+    cmsRunArgData2018D
 ]
 
 TTWZ = [
     'TTWZ',
     '/TTWZ_TuneCP5_13TeV-madgraph-pythia8/dildick-crab_TTWZ_TuneCP5_13TeV-madgraph-pythia8_Filter20201027_V3-463ea116673913e009b1f501c2d5b5ef/USER',
-    cmsRunConfig + cmsRunArgMC2018
+    cmsRunArgMC2018
     ]
 
 TTZToLLNuNu = [
     'TTZToLLNuNu',
     '/TTZToLLNuNu_M-10_TuneCP5_13TeV-amcatnlo-pythia8/dildick-crab_TTZToLLNuNu_M-10_TuneCP5_13TeV-amcatnlo-pythia8_Filter20201027_V3-463ea116673913e009b1f501c2d5b5ef/USER',
-    cmsRunConfig + cmsRunArgMC2018
+    cmsRunArgMC2018
 ]
 
 TTZZ = [
     'TTZZ',
     '/TTZZ_TuneCP5_13TeV-madgraph-pythia8/dildick-crab_TTZZ_TuneCP5_13TeV-madgraph-pythia8_Filter20201027_V3-463ea116673913e009b1f501c2d5b5ef/USER',
-    cmsRunConfig + cmsRunArgMC2018
+    cmsRunArgMC2018
 ]
 
 WWZTo4L2Nu = [
     'WWZTo4L2Nu',
     '/WWZTo4L2Nu_4f_TuneCP5_13TeV_amcatnlo_pythia8/dildick-crab_WWZTo4L2Nu_4f_TuneCP5_13TeV_amcatnlo_pythia8_Filter20201027_V3-463ea116673913e009b1f501c2d5b5ef/USER',
-    cmsRunConfig + cmsRunArgMC2018
+    cmsRunArgMC2018
 ]
 
 WZTo3LNu = [
    'WZTo3LNu',
     '/WZTo3LNu_TuneCP5_13TeV-amcatnloFXFX-pythia8/dildick-crab_WZTo3LNu_TuneCP5_13TeV-amcatnloFXFX-pythia8_Filter20201027_V3-463ea116673913e009b1f501c2d5b5ef/USER',
-    cmsRunConfig + cmsRunArgMC2018
+    cmsRunArgMC2018
 ]
 
 WZZ = [
     'WZZ',
     '/WZZ_TuneCP5_13TeV-amcatnlo-pythia8/dildick-crab_WZZ_TuneCP5_13TeV-amcatnlo-pythia8_Filter20201027_V3-463ea116673913e009b1f501c2d5b5ef/USER',
-    cmsRunConfig + cmsRunArgMC2018
+    cmsRunArgMC2018
 ]
 
 ZZTo4L = [
     'ZZTo4L',
     '/ZZTo4L_TuneCP5_13TeV-amcatnloFXFX-pythia8/dildick-crab_ZZTo4L_TuneCP5_13TeV-amcatnloFXFX-pythia8_Filter20201027_V3-463ea116673913e009b1f501c2d5b5ef/USER',
-    cmsRunConfig + cmsRunArgMC2018
+    cmsRunArgMC2018
 ]
 
 ZZZ = [
     'ZZZ',
     '/ZZZ_TuneCP5_13TeV-amcatnlo-pythia8/dildick-crab_ZZZ_TuneCP5_13TeV-amcatnlo-pythia8_Filter20201027_V3-463ea116673913e009b1f501c2d5b5ef/USER',
-    cmsRunConfig + cmsRunArgMC2018
+    cmsRunArgMC2018
 ]
 
 samples = [
@@ -109,7 +109,8 @@ if __name__ == '__main__':
 
     # process all samples
     for sample in samples:
-        config.JobType.psetName = sample[2]
+        config.JobType.psetName = cmsRunConfig
+        config.JobType.pyCfgParams = sample[2].split(' ')
         config.Data.inputDataset = sample[1]
         config.General.requestName = sample[0] + '_PATANA20201110_v1'
         print config
