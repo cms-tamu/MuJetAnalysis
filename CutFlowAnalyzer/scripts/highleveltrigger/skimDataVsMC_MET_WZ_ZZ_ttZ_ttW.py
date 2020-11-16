440924209cdea6688535c2cb886ec452eb872db4
Sven Dildick <sven.dildick@cern.ch>
2020-11-16 16:29:09 -0600

    update

--- CutFlowAnalyzer/scripts/highleveltrigger/skimDataVsMC_MET_WZ_ZZ_ttZ_ttW.py
+++ CutFlowAnalyzer/scripts/highleveltrigger/skimDataVsMC_MET_WZ_ZZ_ttZ_ttW.py
@@ -26,11 +26,11 @@ verbose = True
 def getPT(m1):
     px = m1[1]
     py = m1[2]
-    return m.sqrt(px*px + py*py)
+    return numpy.sqrt(px*px + py*py)
 
 def getPhi(m1):
     return m1[4]
-    
+
 def normalizePhi(result):
     while (result > M_PI):
         result -= 2*M_PI;
@@ -40,7 +40,7 @@ def normalizePhi(result):
 
 ## total energy of the muon
 def energy(mmu, px, py, pz):
-    return m.sqrt(mmu*mmu + px*px + py*py + pz*pz)
+    return numpy.sqrt(mmu*mmu + px*px + py*py + pz*pz)
 
 ## dimuon invariant mass
 def inner(m1, m2):
@@ -50,7 +50,7 @@ def invmass(m1, m2):
     a = inner(m1,m1)
     b = inner(m2,m2)
     c = 2*inner(m1,m2)
-    return m.sqrt(a+b+c)
+    return numpy.sqrt(a+b+c)
 
 def inv3mass(m1, m2, m3):
     a = inner(m1,m1)
@@ -59,7 +59,7 @@ def inv3mass(m1, m2, m3):
     d = 2*inner(m1,m2)
     e = 2*inner(m1,m3)
     f = 2*inner(m2,m3)
-    return m.sqrt(a+b+c+d+e+f)
+    return numpy.sqrt(a+b+c+d+e+f)
 
 def isMassInZPeak(m):
     return abs(m-mZ)<15
@@ -77,8 +77,18 @@ def bestMassInZPeak(m1, m2):
     return masses[index]
 
 def wasTriggered(tree):
+    isTriggered = False
     ## was triggered?
-    return any('HLT_TrkMu15_DoubleTrkMu5NoFiltersNoVtx' in s for s in list(tree.hltPaths))
+    signalTriggers = [
+        "HLT_TrkMu12_DoubleTrkMu5NoFiltersNoVtx",
+        "HLT_DoubleL2Mu23NoVtx_2Cha",
+        "HLT_TripleMu_12_10_5",
+        "HLT_Mu18_Mu9_SameSign"
+    ]
+    for trigger in signalTriggers:
+        if any(trigger in s for s in list(tree.hltPaths)):
+            isTriggered = True
+    return isTriggered
 
 datasets = ['WZTo3LNu', 'ZZTo4Nu','TTWJetsToLNu', 'TTZJetsToLNu', 'METData']
 
@@ -90,8 +100,7 @@ def add_ZZ(dataFiles):
 
 def add_WZ(dataFiles):
     dataFiles['WZTo3LNu'] = [
-        '/fdata/hepx/store/user/dildick/WZTo3LNu_TuneCUETP8M1_13TeV-powheg-pythia8/crab_WZTo3LNu_Inclusive_ANA_v2/180227_231243/0000/'
-        '/fdata/hepx/store/user/dildick/WZTo3LNu_TuneCUETP8M1_13TeV-powheg-pythia8/crab_WZTo3LNu_Inclusive_ANA_v2/180227_231243/0001/'
+        '/uscms_data/d3/dildick/work/DisplacedMuonJetAnalysis_Run2/CMSSW_10_2_18/src/MuJetAnalysis/CutFlowAnalyzer/scripts/highleveltrigger/out_ana_WZ_2018.root'
         ]
 
 def add_TTW(dataFiles):
@@ -101,7 +110,7 @@ def add_TTW(dataFiles):
 
 def add_TTZ(dataFiles):
     dataFiles['TTZJetsToLNu'] = [
-        '/fdata/hepx/store/user/dildick/TTZToLLNuNu_M-10_TuneCUETP8M1_13TeV-amcatnlo-pythia8/crab_TTbarZ_ANA_v2/180221_004015/0000/'
+        '/uscms_data/d3/dildick/work/DisplacedMuonJetAnalysis_Run2/CMSSW_10_2_18/src/MuJetAnalysis/CutFlowAnalyzer/scripts/highleveltrigger/out_ana_TTZ_2018.root'
         ]
 
 def add_METData(dataFiles):
@@ -116,60 +125,24 @@ add_WZ(dataFiles)
 #add_METData(dataFiles)
 
 MET_Triger_Patterns = [
-    'HLT_MonoCentralPFJet80_PFMETNoMu110_PFMHTNoMu110_IDTight',
-    'HLT_MET75_IsoTrk50',
-    'HLT_MonoCentralPFJet80_PFMETNoMu90_PFMHTNoMu90_IDTight',
-    'HLT_PFMET100_PFMHT100_IDTight',
-    'HLT_MET200',
-    'HLT_PFMET170_BeamHaloCleaned',
-    'HLT_PFMET120_PFMHT120_IDTight',
-    'HLT_Photon50_R9Id90_HE10_Iso40_EBOnly_PFMET40',
-    'HLT_PFMET170_NotCleaned',
-    'HLT_PFMET600',
-    'HLT_PFMETNoMu120_PFMHTNoMu120_IDTight',
-    'HLT_CaloMHTNoPU90_PFMET90_PFMHT90_IDTight',
-    'HLT_LooseIsoPFTau50_Trk30_eta2p1_MET90',
+    'HLT_PFMET100_PFMHT100_IDTight_CaloBTagDeepCSV_3p1',
+    'HLT_PFMET100_PFMHT100_IDTight_PFHT60',
+    'HLT_PFMET110_PFMHT110_IDTight_CaloBTagDeepCSV_',
     'HLT_PFMET110_PFMHT110_IDTight',
-    'HLT_CaloMHTNoPU90_PFMET90_PFMHT90_IDTight_BTagCSV_p067',
-    'HLT_MonoCentralPFJet80_PFMETNoMu100_PFMHTNoMu100_IDTight',
-    'HLT_Photon120_R9Id90_HE10_Iso40_EBOnly_PFMET40',
-    'HLT_MET90_IsoTrk50',
-    'HLT_PFMETNoMu90_PFMHTNoMu90_IDTight',
-    'HLT_PFMET170_JetIdCleaned',
-    'HLT_MET300',
-    'HLT_PFMET90_PFMHT90_IDTight',
-    'HLT_Photon22_R9Id90_HE10_Iso40_EBOnly_PFMET40',
-    'HLT_LooseIsoPFTau50_Trk30_eta2p1_MET80',
-    'HLT_PFHT300_PFMET100',
-    'HLT_PFMET300',
-    'HLT_DiPFJet40_DEta3p5_MJJ600_PFMETNoMu140',
-    'HLT_PFMET100_PFMHT100_IDTight_BeamHaloCleaned',
-    'HLT_DiPFJet40_DEta3p5_MJJ600_PFMETNoMu80',
-    'HLT_PFHT300_PFMET110',
-    'HLT_MET250',
-    'HLT_LooseIsoPFTau50_Trk30_eta2p1_MET120',
-    'HLT_PFMET170_HBHE_BeamHaloCleaned',
-    'HLT_Photon135_PFMET100',
-    'HLT_Ele15_IsoVVVL_PFHT400_PFMET50',
-    'HLT_PFMETNoMu110_PFMHTNoMu110_IDTight',
-    'HLT_Photon36_R9Id90_HE10_Iso40_EBOnly_PFMET40',
-    'HLT_DiCentralPFJet55_PFMET110',
-    'HLT_Ele15_IsoVVVL_PFHT350_PFMET50',
-    'HLT_MET60_IsoTrk35_Loose',
-    'HLT_PFMETNoMu100_PFMHTNoMu100_IDTight',
-    'HLT_PFMETTypeOne190_HBHE_BeamHaloCleaned',
-    'HLT_PFMET400',
-    'HLT_LooseIsoPFTau50_Trk30_eta2p1_MET110',
-    'HLT_MonoCentralPFJet80_PFMETNoMu120_PFMHTNoMu120_IDTight',
-    'HLT_PFMET500',
-    'HLT_PFMET170_HBHECleaned',
-    'HLT_MET600',
-    'HLT_PFMET120_BTagCSV_p067',
-    'HLT_Photon90_R9Id90_HE10_Iso40_EBOnly_PFMET40',
-    'HLT_MET700',
-    'HLT_Photon75_R9Id90_HE10_Iso40_EBOnly_PFMET40',
-    'HLT_PFMET170_NoiseCleaned',
-    ]
+    'HLT_PFMET120_PFMHT120_IDTight_CaloBTagDeepCSV_3p1',
+    'HLT_PFMET120_PFMHT120_IDTight_PFHT60',
+    'HLT_PFMET120_PFMHT120_IDTight',
+    'HLT_PFMET130_PFMHT130_IDTight_CaloBTagDeepCSV_3p1',
+    'HLT_PFMET130_PFMHT130_IDTight',
+    'HLT_PFMET140_PFMHT140_IDTight_CaloBTagDeepCSV_3p1',
+    'HLT_PFMET140_PFMHT140_IDTight',
+    'HLT_PFMET200_HBHECleaned',
+    'HLT_PFMET200_HBHE_BeamHaloCleaned',
+    'HLT_PFMET200_NotCleaned',
+    'HLT_PFMET250_HBHECleaned',
+    'HLT_PFMET300_HBHECleaned',
+    'HLT_PFMETTypeOne100_PFMHT100_IDTight_PFHT60',
+]
 
 def wasMETTriggered(MET_Triger_Patterns, tree):
     for s in MET_Triger_Patterns:
@@ -190,13 +163,10 @@ inputFile = list(sys.argv)[1]
 print inputFile
 rootFile = ROOT.TFile(inputFile,"")
 
-tree = rootFile.Get("cutFlowAnalyzerPXBL3PXFL2/Events")
-#arr = root2array(inputFile, 'cutFlowAnalyzerPXBL3PXFL2/Events')
-#print arr
-#exit(1)
+tree = rootFile.Get("cutFlowAnalyzerPXBL4PXFL3/Events")
 
 newTitle = inputFile#rootFile.GetTitle()
-print 
+print
 newTitle = newTitle.replace('out_ana','out_ana_clone')
 print newTitle
 print tree
@@ -216,68 +186,72 @@ newtree.Branch( 'm123', m123, 'm123/D')
 
 for k in range(0, tree.GetEntries()):
 
-    if k%100==0: print "Processing event ", k, "/",tree.GetEntries()
+    if k%1==0: print "Processing event ", k, "/",tree.GetEntries()
     tree.GetEntry(k)
-    
-    q0 = tree.selMu0_q
-    q1 = tree.selMu1_q
-    q2 = tree.selMu2_q
-    
-    py0 = tree.selMu0_py
-    py1 = tree.selMu1_py
-    py2 = tree.selMu2_py
-    
-    pz0 = tree.selMu0_pz
-    pz1 = tree.selMu1_pz
-    pz2 = tree.selMu2_pz
-    
+
+    q0 = tree.selMu0_charge
+    q1 = tree.selMu1_charge
+    q2 = tree.selMu2_charge
+
+    if q0 == q1 == q2:
+        print "\tSkip"
+        continue
+
     px0 = tree.selMu0_px
     px1 = tree.selMu1_px
     px2 = tree.selMu2_px
-    
-    E0 = energy(mmu, tree.selMu0_px, py0, pz0) 
+
+    py0 = tree.selMu0_py
+    py1 = tree.selMu1_py
+    py2 = tree.selMu2_py
+
+    pz0 = tree.selMu0_pz
+    pz1 = tree.selMu1_pz
+    pz2 = tree.selMu2_pz
+
+    E0 = energy(mmu, tree.selMu0_px, py0, pz0)
     E1 = energy(mmu, px1, py1, pz1)
     E2 = energy(mmu, px2, py2, pz2)
 
     mu0 = [E0, tree.selMu0_px, py0, pz0]
     mu1 = [E1, px1, py1, pz1]
     mu2 = [E2, px2, py2, pz2]
-    
+
     invm3 = inv3mass(mu0, mu1, mu2)
     m123[0] = invm3
-    print m123[0], type(invm3)
+    #print m123[0], type(invm3)
 
     SS_dimuon_mass = 0
     OS_dimuon_mass1 = 0
     OS_dimuon_mass2 = 0
 
-    if q0 * q1 > 0: 
+    if q0 * q1 > 0:
         SS_dimuon_mass = invmass(mu0, mu1)
         OS_dimuon_mass1 = invmass(mu1, mu2)
         OS_dimuon_mass2 = invmass(mu0, mu2)
         OS_dimuon_mass = bestMassInZPeak(OS_dimuon_mass1, OS_dimuon_mass2)
-                
-    elif q0 * q2 > 0: 
+
+    elif q0 * q2 > 0:
         SS_dimuon_mass = invmass(mu0, mu2)
         OS_dimuon_mass1 = invmass(mu0, mu1)
         OS_dimuon_mass2 = invmass(mu1, mu2)
         OS_dimuon_mass = bestMassInZPeak(OS_dimuon_mass1, OS_dimuon_mass2)
-                
-    elif q1 * q2 > 0: 
+
+    elif q1 * q2 > 0:
         SS_dimuon_mass = invmass(mu1, mu2)
         OS_dimuon_mass1 = invmass(mu0, mu1)
         OS_dimuon_mass2 = invmass(mu0, mu2)
         OS_dimuon_mass = bestMassInZPeak(OS_dimuon_mass1, OS_dimuon_mass2)
-        
-    else: 
+
+    else:
         SS_dimuon_mass = 0
         OS_dimuon_mass1 = 0
         OS_dimuon_mass2 = 0
         OS_dimuon_mass = 0
-        
-    best_OS_dimuon_mass[0] = 0 
+
+    best_OS_dimuon_mass[0] = 0
     best_SS_dimuon_mass[0] = 0
-    best_OS_dimuon_mass[0] = OS_dimuon_mass 
+    best_OS_dimuon_mass[0] = OS_dimuon_mass
     best_SS_dimuon_mass[0] = SS_dimuon_mass
 
     OK_MET[0] = 0
@@ -287,7 +261,7 @@ for k in range(0, tree.GetEntries()):
     OK_Signal[0] = 0
     if wasTriggered(tree):
         OK_Signal[0] = 1
-        
+
     newtree.Fill()
 
 newfile.Write()
